03c4b3064b276a7b952e853d8dcb9f5aaf64d43c
reynn211 <144656505+reynn211@users.noreply.github.com>
2024-09-20 19:57:49 +0000

    upd 1

--- 1/eliminate_unset_bits.cpp
+++ 1/eliminate_unset_bits.cpp
@@ -5,8 +5,8 @@ long eliminate_unset_bits(std::string number) {
     long result = 0;
     int power = 0;
     
-    for (auto it = number.rbegin(); it != number.rend(); ++it) {
-        if (*it == '1' && power < 8) {
+    for (int i = 0; i < number.length(); ++i) {
+        if (number[i] == '1' && power < 8) {
             result += std::pow(2, power);
             power++;
         }
--- 1/tests.cpp
+++ 1/tests.cpp
@@ -2,22 +2,44 @@
 #include "eliminate_unset_bits.h"
 
 TEST(EliminateUnsetBitsTest, BasicTests) {
-    EXPECT_EQ(eliminate_unset_bits("1101010101010101"), 255);
-    EXPECT_EQ(eliminate_unset_bits("111"), 7);
-    EXPECT_EQ(eliminate_unset_bits("1000000"), 1);
-    EXPECT_EQ(eliminate_unset_bits("000"), 0);
+    std::string input1 = "1101010101010101";
+    std::string input2 = "111";
+    std::string input3 = "1000000";
+    std::string input4 = "000";
+
+    long result1 = eliminate_unset_bits(input1);
+    long result2 = eliminate_unset_bits(input2);
+    long result3 = eliminate_unset_bits(input3);
+    long result4 = eliminate_unset_bits(input4);
+
+    EXPECT_EQ(result1, 255);
+    EXPECT_EQ(result2, 7);
+    EXPECT_EQ(result3, 1);
+    EXPECT_EQ(result4, 0);
 }
 
 TEST(EliminateUnsetBitsTest, EmptyString) {
-    EXPECT_EQ(eliminate_unset_bits(""), 0);
+    std::string input = "";
+
+    long result = eliminate_unset_bits(input);
+
+    EXPECT_EQ(result, 0);
 }
 
 TEST(EliminateUnsetBitsTest, AllOnes) {
-    EXPECT_EQ(eliminate_unset_bits("11111"), 31);
+    std::string input = "11111";
+
+    long result = eliminate_unset_bits(input);
+
+    EXPECT_EQ(result, 31);
 }
 
 TEST(EliminateUnsetBitsTest, AllZeros) {
-    EXPECT_EQ(eliminate_unset_bits("0000000"), 0);
+    std::string input = "0000000";
+
+    long result = eliminate_unset_bits(input);
+
+    EXPECT_EQ(result, 0);
 }
 
 int main(int argc, char **argv) {
