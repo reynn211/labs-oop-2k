d43297f529bb6b3347db5518f71215389e482d4e
reynn211 <144656505+reynn211@users.noreply.github.com>
2024-09-21 07:54:17 +0000

    fix

--- 1/tests.cpp
+++ 1/tests.cpp
@@ -1,50 +1,50 @@
 #include <gtest/gtest.h>
 #include "eliminate_unset_bits.h"
 
-TEST(EliminateUnsetBitsTest, BasicTests) {
-    std::string input1 = "1101010101010101";
-    std::string input2 = "111";
-    std::string input3 = "1000000";
-    std::string input4 = "000";
+TEST(EliminateUnsetBitsTest, BasicTest1) {
+    std::string input = "1101010101010101";
+    long result = eliminate_unset_bits(input);
+    EXPECT_EQ(result, 255);
+}
 
-    long result1 = eliminate_unset_bits(input1);
-    long result2 = eliminate_unset_bits(input2);
-    long result3 = eliminate_unset_bits(input3);
-    long result4 = eliminate_unset_bits(input4);
+TEST(EliminateUnsetBitsTest, BasicTest2) {
+    std::string input = "111";
+    long result = eliminate_unset_bits(input);
+    EXPECT_EQ(result, 7);
+}
 
-    EXPECT_EQ(result1, 255);
-    EXPECT_EQ(result2, 7);
-    EXPECT_EQ(result3, 1);
-    EXPECT_EQ(result4, 0);
+TEST(EliminateUnsetBitsTest, BasicTest3) {
+    std::string input = "1000000";
+    long result = eliminate_unset_bits(input);
+    EXPECT_EQ(result, 1);
+}
+
+TEST(EliminateUnsetBitsTest, BasicTest4) {
+    std::string input = "000";
+    long result = eliminate_unset_bits(input);
+    EXPECT_EQ(result, 0);
 }
 
 TEST(EliminateUnsetBitsTest, EmptyString) {
     std::string input = "";
-
     long result = eliminate_unset_bits(input);
-
     EXPECT_EQ(result, 0);
 }
 
 TEST(EliminateUnsetBitsTest, AllOnes) {
     std::string input = "11111";
-
     long result = eliminate_unset_bits(input);
-
     EXPECT_EQ(result, 31);
 }
 
 TEST(EliminateUnsetBitsTest, AllZeros) {
     std::string input = "0000000";
-
     long result = eliminate_unset_bits(input);
-
     EXPECT_EQ(result, 0);
 }
 
 TEST(EliminateUnsetBitsTest, InvalidInput) {
     std::string input = "1234567";
-
     EXPECT_THROW(eliminate_unset_bits(input), std::invalid_argument);
 }
 
